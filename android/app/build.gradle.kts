9370978904a81eefd8dd2dedc1759f104a4dffb5
Van Nguyen <nguyenthanhvan1902@gmail.com>
2026-07-28 22:02:07 +0700

    fix: set Gradle 8+ namespace in build.gradle.kts and remove package attribute from AndroidManifest.xml

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,11 +1,10 @@
 plugins {
     id("com.android.application")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.example.aegis_net"
+    namespace = "com.aegisnet.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -15,10 +14,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.aegis_net"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.aegisnet.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -27,8 +23,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -40,6 +34,4 @@ kotlin {
     }
 }
 
-flutter {
-    source = "../.."
-}
+dependencies {}
